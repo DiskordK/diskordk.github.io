710ac6fcd8bd48eec1a163e7e9f671a84d10c1d8
DiskordK <64530768+DiskordK@users.noreply.github.com>
2020-08-09 22:06:39 +0500

    test
    1

---
+++ code0.js
@@ -0,0 +1,49 @@
+gdjs.New_32sceneCode = {};
+gdjs.New_32sceneCode.GDgroundObjects1= [];
+gdjs.New_32sceneCode.GDgroundObjects2= [];
+gdjs.New_32sceneCode.GDground2Objects1= [];
+gdjs.New_32sceneCode.GDground2Objects2= [];
+gdjs.New_32sceneCode.GDsky_951Objects1= [];
+gdjs.New_32sceneCode.GDsky_951Objects2= [];
+gdjs.New_32sceneCode.GDCoinObjects1= [];
+gdjs.New_32sceneCode.GDCoinObjects2= [];
+gdjs.New_32sceneCode.GDCharacterObjects1= [];
+gdjs.New_32sceneCode.GDCharacterObjects2= [];
+
+gdjs.New_32sceneCode.conditionTrue_0 = {val:false};
+gdjs.New_32sceneCode.condition0IsTrue_0 = {val:false};
+
+
+gdjs.New_32sceneCode.eventsList0 = function(runtimeScene) {
+
+{
+
+
+{
+}
+
+}
+
+
+};
+
+gdjs.New_32sceneCode.func = function(runtimeScene) {
+runtimeScene.getOnceTriggers().startNewFrame();
+
+gdjs.New_32sceneCode.GDgroundObjects1.length = 0;
+gdjs.New_32sceneCode.GDgroundObjects2.length = 0;
+gdjs.New_32sceneCode.GDground2Objects1.length = 0;
+gdjs.New_32sceneCode.GDground2Objects2.length = 0;
+gdjs.New_32sceneCode.GDsky_951Objects1.length = 0;
+gdjs.New_32sceneCode.GDsky_951Objects2.length = 0;
+gdjs.New_32sceneCode.GDCoinObjects1.length = 0;
+gdjs.New_32sceneCode.GDCoinObjects2.length = 0;
+gdjs.New_32sceneCode.GDCharacterObjects1.length = 0;
+gdjs.New_32sceneCode.GDCharacterObjects2.length = 0;
+
+gdjs.New_32sceneCode.eventsList0(runtimeScene);
+return;
+
+}
+
+gdjs['New_32sceneCode'] = gdjs.New_32sceneCode;
